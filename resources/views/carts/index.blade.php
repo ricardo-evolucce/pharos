d9d04c93e1241342929a1c1f45b9a109be48415c
ricardo <ricardo.evolucce@gmail.com>
2021-09-03 10:02:44 -0300

    exclusao de agenciados e de carrinhos

--- resources/views/carts/index.blade.php
+++ resources/views/carts/index.blade.php
@@ -98,13 +98,13 @@
                                     </a>
                                 </div>
                                      <div class="btn-group">
-                                <!--<form action="{{ route('carts.delete') }}" method="POST" onSubmit="return confirm('Apagar carrinho?');">
+                               <form action="{{ route('carts.delete') }}" method="POST" onSubmit="return confirm('Apagar carrinho?');">
                                     @csrf
                                     <input type="hidden" name="id" value="{{$cart->id}}" />
                                     <button type="submit" class="btn btn-sm btn-danger" data-toggle="tooltip" title="Apagar" data-placement="left">
                                         <i class="fa fa-trash"></i>
                                     </button>
-                                </form>-->
+                                </form>
 
                             </div>
                             </td>
