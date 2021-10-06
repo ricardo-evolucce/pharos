91795ca936909d4d28a9fc5e14f5090bdcd06566
cs <gelson@centralsystem.com.br>
2021-10-05 22:37:01 -0300

    Componentes de edicao de carrinho

---
+++ resources/assets/js/laravel/components/ProfileListEdit.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="form-group row items-push mb-0">
+    <ProfileItemEdit :ref="'profile_' + profile.id" v-for="(profile, index) in profiles" :index="index" :profile="profile" :key="profile.id"  :profiles_select ="profiles_select"  :profiles_ids ="profiles_ids"></ProfileItemEdit>
+  </div>
+</template>
+
+<script>
+
+import ProfileItemEdit from './ProfileItemEdit';
+import ProfileFilter from './ProfileFilter';
+
+export default {
+  name: "ProfileListEdit",
+  components: {
+    ProfileItemEdit,
+    ProfileFilter
+  },
+  props: {
+    profiles_select: Array,
+    profiles_ids: Array,
+  },
+  created() {
+    this.$store.dispatch('getProfiles');
+  },
+  computed: {
+    profiles() {
+      return this.$store.state.profileList
+    }
+  }
+}
+</script>
