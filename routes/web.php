1d49c1089fcb1f31632c8e335b76c8ff81631596
cs <gelson@centralsystem.com.br>
2021-11-17 11:41:41 -0300

    debug

--- routes/web.php
+++ routes/web.php
@@ -80,6 +80,7 @@ Route::group(['middleware' => ['auth']], function () {
 
     // Cart
     Route::get('/carts/pendings', 'CartController@pendings')->name('carts.pendings');
+    Route::post('/carts/storeCartDraft', 'CartController@storeCartDraft')->name('carts.storeCartDraft');
     Route::resource('/carts', 'CartController');
     Route::get('/carts/{cart}/preview/{profile}', 'CartController@previewPDF')->name('profile.preview');
     Route::get('/carts/{cart}/send', 'CartController@sendCart')->name('carts.send');
@@ -88,7 +89,6 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('/carts/update/{cart}', 'CartController@update')->name('carts.update');
     Route::post('/carts/updateItemPhoto/{cart}', 'CartController@updateItemPhoto')->name('carts.updateItemPhoto');
     Route::post('/carts/duplicate', 'CartController@duplicate')->name('carts.duplicate');
-    Route::post('/carts/storeCartDraft', 'CartController@storeCartDraft')->name('carts.storeCartDraft');
 
     // Profile
     Route::resource('/profiles', 'ProfileController');
