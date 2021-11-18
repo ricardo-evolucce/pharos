81b9e38ca143f7a61dd02f5fb09ffadc340dd952
cs <gelson@centralsystem.com.br>
2021-11-18 01:40:32 -0300

    debug

--- resources/assets/js/laravel/components/Views/CartCreate.vue
+++ resources/assets/js/laravel/components/Views/CartCreate.vue
@@ -403,9 +403,9 @@ export default {
       this.profile_id = []
 
       if(this.cart_id){
+        this.selectedImages = []
         this.getUserPhotosCarts(this.cart_id)
           .then(cart_select_photos => {
-            this.selectedImages = []
               for (const i in cart_select_photos) {
                   const fotos = cart_select_photos[i];
                   for (const j in fotos) {
